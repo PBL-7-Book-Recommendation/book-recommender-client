b941b684a1b0024cabd2375c182a8b7295d32a94
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2024-05-16 21:02:53 +0700

    implement UI for about us page

---
+++ src/data/aboutUs.ts
@@ -0,0 +1,27 @@
+export const ABOUT_US = {
+	title: "About Us",
+	whoWeAre:
+		"We are a group of four passionate bookworms from Danang University Of Science and Technology (DUT) who came together for a project-based learning experience. We believe that everyone deserves to discover the joy of reading, and with the ever-growing ocean of books available, finding that perfect read can be overwhelming. That's where BookHub comes in!",
+	whatWeDo:
+		"BookHub utilizes the power of machine learning to analyze your reading preferences and suggest books you'll truly enjoy. We go beyond simple genre recommendations by considering factors like writing style, thematic elements, and author popularity.",
+	story:
+		"BookHub was born out of our shared love for reading and our desire to create a tool that would help others find their next favorite book. We started off by researching existing book recommendation systems and exploring different machine learning algorithms. With countless late-night coding sessions and fueled by our shared enthusiasm, BookHub began to take shape. ",
+	team: [
+		{
+			name: "Bui Hong Phuoc",
+			role: "Team Leader",
+		},
+		{
+			name: "Nguyen Huu Huy",
+			role: "Front-end Developer",
+		},
+		{
+			name: "Tran Thi Ngoc Tram",
+			role: "Back-end Developer",
+		},
+		{
+			name: "Tran Nhat Minh",
+			role: "AI Developer",
+		},
+	],
+};
