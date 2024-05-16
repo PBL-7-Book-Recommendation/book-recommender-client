398f9eb45af7c9b7ba435477b36bab72081f493e
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2024-05-16 21:10:33 +0700

    implement UI for bookshelves

---
+++ src/layouts/BookShelves/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const BookShelves = () => {
+	return <Box maxWidth="lg" paddingY={"1rem"} margin={"auto"}></Box>;
+};
+
+export default BookShelves;
